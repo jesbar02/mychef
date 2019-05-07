aa4039e1c97abeacdfdd389fb019a06b6ef7a444
jhernandez <jesus.hernandez@blackboard.com>
2019-05-06 20:06:17 -0500

    included wraper cookbook mychef-client

---
+++ cookbooks/mychef-client/recipes/default.rb
@@ -0,0 +1,10 @@
+#
+# Cookbook:: mychef-client
+# Recipe:: default
+#
+# Copyright:: 2019, The Authors, All Rights Reserved.
+
+node.default['chef_client']['interval'] = 1200
+node.default['chef_client']['splay'] = 1200
+
+include_recipe 'chef-client::default'
